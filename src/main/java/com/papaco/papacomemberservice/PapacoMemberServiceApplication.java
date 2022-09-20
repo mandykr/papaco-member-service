00f6803ef9fb68397904d6ba224c4c7019925041
mandykr <hongseokii92@gmail.com>
2022-09-20 15:18:47 +0900

    Initial commit

---
+++ src/main/java/com/papaco/papacomemberservice/PapacoMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.papaco.papacomemberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapacoMemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapacoMemberServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/papaco/papacomemberservice/PapacoMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaco.papacomemberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapacoMemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
